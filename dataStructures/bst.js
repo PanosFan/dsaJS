7ff60ec61a873ef7bdaffc9e610c83542b3924fa
Panagiotis <panfanartzis@gmail.com>
2022-07-01 00:44:58 +0300

    bfs for bst

--- dataStructures/bst.js
+++ dataStructures/bst.js
@@ -121,4 +121,18 @@ class Bst {
       }
     }
   }
+
+  breadthFirstSearch() {
+    let currentNode = this.root;
+    let result = [];
+    let queue = [];
+    queue.push(currentNode);
+    while (queue.length) {
+      currentNode = queue.shift();
+      result.push(currentNode.value);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+    return result;
+  }
 }
